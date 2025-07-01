b0f84bcc2a10fbadef94cc09c0e93a675a3a21ab
zyumax <kazuma_sge@icloud.com>
2025-07-01 23:16:28 +0900

    課題4

---
+++ jestSample/jest4/jeat4_1/prefecture.ts
@@ -0,0 +1,43 @@
+import { REGION_GROUPS, RegionGroup } from './region';
+import axios from "axios";
+
+type ZipCloudResponse = {
+  status: number;
+  message: string | null;
+  results: {
+    address1: string;
+  }[];
+};
+
+// APIで都道府県を取得する
+async function getPrefecture(postalCode: string): Promise<string> {
+  const url = `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${postalCode}`;
+  const response = await axios.get<ZipCloudResponse>(url);
+  if (response.data.status !== 200 || !response.data.results?.[0]) {
+    throw new Error("住所が見つかりませんでした。");
+  }
+
+  return response.data.results[0].address1;
+}
+
+// 都道府県から地方を取得する
+function getRegionGroup(prefecture: string): RegionGroup {
+  const foundRegion = (Object.entries(REGION_GROUPS) as [RegionGroup, readonly string[]][])
+    .find(([region, prefectures]) => prefectures.includes(prefecture));
+
+  if (!foundRegion) {
+    throw new Error(`都道府県「${prefecture}」に対応する地方が見つかりません。`);
+  }
+
+  return foundRegion[0];
+}
+
+// 地方情報のメッセージを返す
+async function getRegionMessage(postalCode: string): Promise<string> {
+  const prefecture = await getPrefecture(postalCode);
+  const region = getRegionGroup(prefecture);
+  
+  return (`${prefecture} は ${region} です`);
+}
+
+getRegionMessage("1000001");
